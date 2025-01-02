fc39f7928537750eadea6faaa4561888fb5830ae
Fepozopo <fepozopo.f39@gmail.com>
2025-01-01 18:29:20 -0800

    Updated handlerThumbnailGet add the thumbnail to the global map and update the database

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,60 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	// Parse the form data
+	err = r.ParseMultipartForm(10 << 20) // 10 MB
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error parsing form data", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	// Get the file from the form data
+	file, _, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error getting file from form data", err)
+		return
+	}
+	defer file.Close()
+	// Get the media type from the file header
+	mediaType := r.Header.Get("Content-Type")
+
+	// Read the image data into a byte slice using io.ReadAll
+	imageData, err := io.ReadAll(r.Body)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error reading image data", err)
+		return
+	}
+
+	// Get the video's metadata from the SQLite database using GetVideo
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Couldn't get video", err)
+		return
+	}
+	// Check if the user is the owner of the video
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You can't upload a thumbnail for this video", nil)
+		return
+	}
+
+	// Save the thumbnail to the global map
+	videoThumbnails[videoID] = thumbnail{
+		data:      imageData,
+		mediaType: mediaType,
+	}
+
+	// Update the database with the new thumbnail URL using UpdateVideo
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
+	video.ThumbnailURL = &thumbnailURL
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error updating video in database", err)
+		return
+	}
+
+	// Respond with updated JSON of the video's metadata
+	respondWithJSON(w, http.StatusOK, video)
+
 }
